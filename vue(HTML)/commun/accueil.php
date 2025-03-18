226956f91fb1cd22956b304a1ff0b0c06990f313
lu-dos <102653485+lu-dos@users.noreply.github.com>
2025-03-18 12:17:59 +0100

    mama

--- vue(HTML)/commun/accueil.php
+++ vue(HTML)/commun/accueil.php
@@ -14,36 +14,12 @@
 
 <section class="hero">
     <h1>Réserve ta pétanque .com</h1>
-    <a href="reservation.php" class="btn">Réserver maintenant</a>
 </section>
 
 <section>
 <img src="IMG/hero3.png" alt="Terrain de pétanque 2">
 </section>
 
-<!-- <section class="hero-section">
-    <div class="hero_slider">
-        <div class="slide active">
-            <img src="IMG/hero1.jpg" alt="boules sur terrain">
-            <div class="slide-texte">
-                <h1>Des terrains de pétanque à réserver</h1>
-                <p>Réservez votre terrain</p>
-            </div>
-        </div>
-
-        <div class="slide">
-            <img src="IMG/hero2.jpg" alt="Terrain de pétanque 2">
-            <div class="slide-text">
-                <h1>Un concept original</h1>
-            <p>Une idée fun pour animer vos soirées</p>
-        </div>
-
-           <!-sFlèches de navigation (optionnel) -->
-      <!-- <div class="arrow prev">&#10094;</div>
-      <div class="arrow next">&#10095;</div>
-    </div>
-
-</section> --> 
 
 <ul class="nav-links">
         <li><a href="#quisommesnous">Qui sommes-nous ?</a></li>
@@ -53,7 +29,6 @@
       </ul>
 
       <div class="social-links">
-        <a href="#" class="linkedin">LinkedIn</a>
         <a href="#" class="instagram">Instagram</a>
       </div>
 
