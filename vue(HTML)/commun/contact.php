3a73c69d88e433214e9697c3e605db38b01b2c19
lu-dos <102653485+lu-dos@users.noreply.github.com>
2025-06-12 14:05:52 +0200

    Améliore l'interface et ajoute pages informatives

--- vue(HTML)/commun/contact.php
+++ vue(HTML)/commun/contact.php
@@ -10,6 +10,7 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/E5_petanque_MVC/LA_PETANQUE_LA_VRAI/include(redondance)/navbar.php'); ?>
     <div class="container">
         <h1>Contacter le Créateur</h1>
+        <p>N'hésitez pas à nous laisser un message pour toute question ou suggestion.</p>
         <form action="traitement_contact.php" method="POST">
             <label for="name">Nom :</label>
             <input type="text" id="name" name="name" required>
@@ -20,7 +21,7 @@
             <label for="message">Message :</label>
             <textarea id="message" name="message" rows="5" required></textarea>
 
-            <button type="submit">Envoyer</button>
+            <button class="button" type="submit">Envoyer</button>
         </form>
     </div>
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/E5_petanque_MVC/LA_PETANQUE_LA_VRAI/include(redondance)/footer.php'); ?>
