2352b743fee7ad720a6bc9179b4ab574573cf4e9
lu-dos <102653485+lu-dos@users.noreply.github.com>
2025-06-14 15:57:12 +0200

    Add viewport meta tags and responsive navbar

--- vue(HTML)/commun/modifier.php
+++ vue(HTML)/commun/modifier.php
@@ -70,6 +70,7 @@ if (!$terrain) {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modifier le terrain</title>
     <base href="/E5_petanque_MVC/LA_PETANQUE_LA_VRAI/">
     <link rel="stylesheet" href="css/index.css">
--- vue(HTML)/commun/resa2.php
+++ vue(HTML)/commun/resa2.php
@@ -30,6 +30,7 @@ try {
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Réservation de Terrain</title>
     <link rel="stylesheet" href="rstyle.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" />
