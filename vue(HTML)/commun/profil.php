d6e64ec71c7d12189a5039ece2b4d7ea9f898611
lu-dos <102653485+lu-dos@users.noreply.github.com>
2025-06-18 11:38:22 +0200

    Allow profile editing and password change

--- vue(HTML)/commun/profil.php
+++ vue(HTML)/commun/profil.php
@@ -39,11 +39,20 @@ if (!$user) {
 <div class="container">
     <h1>Mon Profil</h1>
     <div class="card">
-        <p><strong>Nom :</strong> <?= htmlspecialchars($user['nom']) ?></p>
-        <p><strong>Prénom :</strong> <?= htmlspecialchars($user['Prenom']) ?></p>
-        <p><strong>Email :</strong> <?= htmlspecialchars($user['mail']) ?></p>
-
-
+        <form method="POST" action="/E5_petanque_MVC/LA_PETANQUE_LA_VRAI/controleur(PHP)/traitement_profil.php">
+            <label>Nom</label>
+            <input type="text" name="nom" value="<?= htmlspecialchars($user['nom']) ?>" required><br>
+            <label>Prénom</label>
+            <input type="text" name="prenom" value="<?= htmlspecialchars($user['Prenom']) ?>" required><br>
+            <label>Mail</label>
+            <input type="email" name="mail" value="<?= htmlspecialchars($user['mail']) ?>" required><br>
+            <hr>
+            <label>Mot de passe actuel</label>
+            <input type="password" name="current_password"><br>
+            <label>Nouveau mot de passe</label>
+            <input type="password" name="new_password"><br>
+            <input type="submit" value="Mettre à jour">
+        </form>
     </div>
 </div>
 </body>
